03967fc6187241bf7bf4bf7cb4b42d3796a91c6f
dangerousCoder-OP <102338123+dangerousCoder-OP@users.noreply.github.com>
2026-01-14 08:44:37 +0530

    Initial commit

---
+++ Loop.py
@@ -0,0 +1,11 @@
+for i in range(1,20): # It will print from 1 to 19 20 is excluded
+    print(i)
+
+x = 10
+for i in range(x): # It will start from 0 and till 9
+    print(i)
+
+x = 1
+while(x < 5):
+    print(x)
+    x += 1
